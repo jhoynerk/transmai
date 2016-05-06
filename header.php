c8fb408a5956411e33b6bb984e17fc547ce8f53b
Jhoynerk Caraballo <jhoynerk@softwarecriollo.com>
2016-05-06 10:32:41 -0430

    add footer

--- header.php
+++ header.php
@@ -14,21 +14,17 @@
   </head>
 
   <body>
-
-    <!-- Fixed navbar -->
     <nav class="navbar navbar-default">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <!-->
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-          </!-->
           </button>
           <a class="navbar-brand nav-icon" href="#"><img class= "img-responsive" src= "images/icon_background.png"></a>
         </div>
-        <div id="" class=" navbar-collapse ">
+        <div class=" navbar-collapse ">
           <ul class="nav navbar-nav navbar-right hidden-xs">
             <li><a class="field-phone" href="tel:11-23335-1000">Tel.: (11) 2335-1000 / 2334-9910</a></li>
             <li><a href="#">EN</a></li>
@@ -37,8 +33,7 @@
             <li><p>/</p></li>
             <li><a href="#">ES</a></li>
           </ul>
-        </div><!--/.nav-collapse -->
-
+        </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
             <li class="active"><a href="#">INICIO</a></li>
